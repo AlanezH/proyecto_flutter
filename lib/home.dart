b6646c4526126818a8e03101aac8a6f1ffdefca6
AlanezH <alaneshilder123@gmail.com>
2025-10-20 10:38:15 -0400

    widgetAssetImage,widgetListView,widgetLinerGradient 20/10/25

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:proyecto_flutter/description_place.dart';
+import 'package:proyecto_flutter/gradient_back.dart';
+import 'package:proyecto_flutter/review.dart';
+import 'package:proyecto_flutter/review_list.dart';
 
 class MyHome extends StatelessWidget{
   @override
@@ -15,18 +18,29 @@ class MyHome extends StatelessWidget{
       ),
       child: DescriptionPlace("Uyuni", 4, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
     );
+    final reviewList =Container(
+      margin: EdgeInsets.only(
+          top: 20,
+          left: 30,
+          right: 30
+      ),
+
+      child: ReviewList()
+    );
+
+    //listView
+    final listView = ListView(
+      children: <Widget>[
+        descriptionPlace,
+        reviewList
+      ],
+    );
 
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Text(
-          "My Places"
-        ),
-      ),
       body:Stack(
         children: <Widget>[
-          descriptionPlace
+          GradientBack()
         ],
       )
     );
